b1829bccb850bbbdaf13c6f43af4502143e54e18
rania <raniamn03@gmail.com>
2024-05-01 19:34:48 +0700

    change icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,21 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icons/apple-touch-icon.png"
+      />
+      <link
         rel="icon"
-        href="/icons/raniaarn.svg"
-        type="image/svg+xml"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
       />
       <body>
         <Main />
