f0dba3ca6367c737ef8576151a8286ad820c7509
rania <raniamn03@gmail.com>
2024-05-02 12:40:06 +0700

    verify domain ownership for seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,26 +3,27 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/icons/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/icons/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/icons/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/site.webmanifest" />
-      <meta name="google-site-verification" content="google-site-verification=2tHTYWSNOYbZ-XLS5PAarS7PdJuWHDXZsqNJ_jo8iYE" />
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="google-site-verification" content="2tHTYWSNOYbZ-XLS5PAarS7PdJuWHDXZsqNJ_jo8iYE" />
+      </Head>
       <body>
         <Main />
         <NextScript />
