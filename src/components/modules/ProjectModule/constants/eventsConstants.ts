02217225ab12502aa1194e259d6d107cceffcab0
rania <raniamn03@gmail.com>
2024-07-12 01:17:06 +0700

    chore: add new data

--- src/components/modules/ProjectModule/constants/eventsConstants.ts
+++ src/components/modules/ProjectModule/constants/eventsConstants.ts
@@ -1,7 +1,43 @@
 export const SUMMARY = {
   title: "Organizations and Events",
-  summary: "My past experiences in several organizations and events. My experiences mostly circles around creative fields, including graphic designs, photography, videography, and UI/UX Designs. ",
+  summary: "My past experiences in several organizations and events. My experiences mostly circles around creative fields, including graphic designs, photography, videography, and UI/UX Designs.",
   deliverables: [
-    "COMPFEST", "OH", "BEM"
+    "COMPFEST", "RUMBEL", "BEM", "OH"
   ],
 }
+
+export const EVENTS = [
+  {
+    title: "COMPFEST",
+    roles: "Manager of Creative",
+    descriptions: [
+      "Led the development of the visual branding for COMPFEST 15 by creating a design system and shaping the team's vision.",
+      "Lead 35 team members encompassing visual design, decoration, and documentation, as well as animation.",
+      "Cooperated with 12 departments to discuss solutions and fulfill creative requests, including website assets and graphic design materials, tailored to their specific needs."
+    ],
+    childs: [
+      {
+        descriptions: "One of my task as the manager of Creative, i had to led and supervised the development of COMPFEST 15's visual branding. Me and my team had to create and experiment concepts. Below is one of the first concept that we explored. The designs were made by me, I also made assets which later also used for the final concept, just had different color palette.",
+        imageUrls: "cf_first.png"
+      },
+      {
+        descriptions: "Eventually, we developed a final concept of a metallic design with an evolving philosophy. We chose the evolving theme by incorporating light and dark modes to embody this concept. The evolving idea was inspired by COMPFEST 15's own theme, #EvolveForTomorrow. As I supervised three divisions (Visual Design, Decoration, and Documentation), we also discussed and defined the themes for each division, which are detailed below. These themes will represent the appearance and essence of our products.",
+        imageUrls: "Compfest_design_concept.png"
+      },
+      {
+        descriptions: "The COMPFEST logo, from editions 6 to 14, has maintained the same shape, varying only in colors and art styles. For COMPFEST 15, we aimed for a metallic and elegant look to emphasize the creative and visual design's concept of being evolving and distinct. While the shape of the logo was created by my team, I was responsible for its color design.",
+        imageUrls: "cf_logo.png"
+      },
+      {
+        descriptions: "You can see the final design below. As the manager, my duty was to ensure quality by checking every design before it was sent to other departments or published on COMPFEST social media. If necessary, I would perfect the design and discuss with the team ways to improve it. The requests were vary, from social medias designs, slides, posters, decorating for offline events (banners, marquee letters, etc), virtual backgrounds (including animation), videos, web assets, and others",
+        imageUrls: "Compfest.png"
+      },
+      {
+        descriptions: "In addition to my duties of creating designs and ensuring all requests are of high quality, I also led the team and coordinated their efforts for every event. This involved leading meetings, guiding the team on the necessary steps, especially at the beginning, and addressing any issues they faced. I focused particularly on resolving issues that involved other departments.",
+        imageUrls: "cf_doc.png"
+      }
+    ]
+  }, {
+
+  }
+]
