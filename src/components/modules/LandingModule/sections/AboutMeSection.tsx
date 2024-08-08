4b5ff8fc08338c26d7b9c5c98b6d9cc10f911e70
rania <raniamn03@gmail.com>
2024-08-08 22:05:43 +0700

    chore: change flex for about me

--- src/components/modules/LandingModule/sections/AboutMeSection.tsx
+++ src/components/modules/LandingModule/sections/AboutMeSection.tsx
@@ -8,7 +8,7 @@ export const AboutMeSection = () => {
 
   return (
     <div id='home-header' className={`pt-32 px-4 md:pt-40`}>
-      <div className='flex-col my-auto md:flex-row flex mx-8 md:mx-20 items-end gap-4'>
+      <div className={`flex my-auto items-end gap-4 mx-8 md:mx-20 ${width >= 1000 ? 'flex-row' : 'flex-col'}`}>
         <div className='flex-col flex items-center md:items-start w-full h-full gap-2'>
           <div className="flex-row flex gap-2 items-center justify-center">
             <div className="text-4xl font-semibold text-white">
@@ -19,13 +19,14 @@ export const AboutMeSection = () => {
               src={'/images/blinks.png'}
               alt={'Raniaarn'}
               width={40}
-              height={40} >
+              height={40}
+            >
             </Image>
           </div>
           <h1 className={`text-4xl font-semibold text-white text-center md:text-left`}>
             I'm <span className='text-purple-400'>Rania Maharani Narendra</span>
           </h1>
-          <p className='text-white text-sm mx-4 pb-4 md:mr-24 md:mx-auto xl:mx-0 text-justify pt-2'>
+          <p className='text-white w-full text-sm mx-4 pb-4 md:mr-24 md:mx-auto xl:mx-0 text-justify pt-2'>
             Currently pursuing my undergraduate studies at the Faculty of Computer Science at the University of Indonesia. My academic and non academic journeys fueled my passion for UI/UX, Graphic Design, and Software Development.
           </p>
           <div className='flex-row pb-6 md:pb-16 flex gap-8 w-full justify-center md:justify-start'>
@@ -33,7 +34,7 @@ export const AboutMeSection = () => {
               <Button content='Contact Me' style='primary' />
             </Link>
             <Link href="https://docs.google.com/document/d/17k2KLO9s-rpLuWoOd6blPUE32Nf_ERBrk_zPkjxWEiY/preview" target='blank'>
-              <Button content='View CV' style='outline'/>
+              <Button content='View CV' style='outline' />
             </Link>
           </div>
         </div>
