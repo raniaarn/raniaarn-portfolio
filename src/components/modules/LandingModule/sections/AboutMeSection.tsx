82b37b25eac932a9ad51583b087df82bc419c647
rania <raniamn03@gmail.com>
2024-08-08 17:07:45 +0700

    chore: change picture for about me

--- src/components/modules/LandingModule/sections/AboutMeSection.tsx
+++ src/components/modules/LandingModule/sections/AboutMeSection.tsx
@@ -8,7 +8,7 @@ export const AboutMeSection = () => {
 
   return (
     <div id='home-header' className={`pt-32 px-4 md:pt-40`}>
-      <div className='flex-col my-auto md:flex-row flex mx-8 md:mx-20 items-end'>
+      <div className='flex-col my-auto md:flex-row flex mx-8 md:mx-20 items-end gap-4'>
         <div className='flex-col flex items-center md:items-start w-full h-full gap-2'>
           <div className="flex-row flex gap-2 items-center justify-center">
             <div className="text-4xl font-semibold text-white">
@@ -25,7 +25,7 @@ export const AboutMeSection = () => {
           <h1 className={`text-4xl font-semibold text-white text-center md:text-left`}>
             I'm <span className='text-purple-400'>Rania Maharani Narendra</span>
           </h1>
-          <p className='text-white text-sm mx-4 pb-4 md:mr-24 md:mx-auto text-justify pt-2'>
+          <p className='text-white text-sm mx-4 pb-4 md:mr-24 md:mx-auto xl:mx-0 text-justify pt-2'>
             Currently pursuing my undergraduate studies at the Faculty of Computer Science at the University of Indonesia. My academic and non academic journeys fueled my passion for UI/UX, Graphic Design, and Software Development.
           </p>
           <div className='flex-row pb-6 md:pb-16 flex gap-8 w-full justify-center md:justify-start'>
@@ -37,7 +37,7 @@ export const AboutMeSection = () => {
             </Link>
           </div>
         </div>
-        <div className='mx-auto relative bottom-0 w-[50%] md:w-[50%] aspect-[4.2/5] overflow-hidden'>
+        <div className='mx-auto relative bottom-0 w-[70%] md:w-[50%] max-w-96 aspect-[4.2/5] overflow-hidden'>
           <Image
             src={'/images/portrait2.png'}
             alt={'Raniaarn'}
