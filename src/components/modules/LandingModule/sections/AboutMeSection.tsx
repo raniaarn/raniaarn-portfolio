acce526d963138284b2db82d964895d65088e874
rania <raniamn03@gmail.com>
2024-12-17 08:47:19 +0700

    perf: update description and title

--- src/components/modules/LandingModule/sections/AboutMeSection.tsx
+++ src/components/modules/LandingModule/sections/AboutMeSection.tsx
@@ -11,7 +11,7 @@ export const AboutMeSection = () => {
       <div className='flex-col my-auto lg:flex-row flex items-end gap-4 max-w-[1200px] mx-auto px-8 md:px-20'>
         <div className='flex-col flex items-center md:items-start w-full h-full gap-2'>
           <div className="flex-row flex gap-2 items-center justify-center">
-            <div className="text-4xl font-semibold text-white">
+            <div className="text-3xl font-semibold text-white">
               Hi!
             </div>
             <Image
@@ -22,11 +22,11 @@ export const AboutMeSection = () => {
               height={40} >
             </Image>
           </div>
-          <h1 className={`text-4xl font-semibold text-white text-center md:text-left`}>
-            I'm <span className='text-purple-400'>Rania Maharani Narendra</span>
+          <h1 className={`text-3xl font-semibold text-white text-center md:text-left`}>
+            Let's <span className='text-purple-400'> Empower</span> Ideas Through Design!
           </h1>
           <p className='text-white w-full text-sm mx-4 pb-4 md:mr-24 md:mx-auto xl:mx-0 text-justify pt-2'>
-          Currently diving deep into Computer Science at the University of Indonesia, I'm on a creative journey where tech meets design. Fueled by a passion for UI/UX, Graphic Design, and Software Development, I turn ideas into impactful, user-friendly experiences!
+          I'm Rania Maharani Narendra, currently diving deep into Computer Science at the University of Indonesia, I'm on a creative journey where tech meets design. Fueled by a passion for UI/UX, Graphic Design, and Software Development, I turn ideas into impactful, user-friendly experiences!
           </p>
           <div className='flex-row pb-6 md:pb-16 flex gap-8 w-full justify-center md:justify-start'>
             <Link href="#contact">
