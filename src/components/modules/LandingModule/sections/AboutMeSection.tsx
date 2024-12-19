76e4685ed85d1deeb777406f1b04fb12a15ae3e1
rania <raniamn03@gmail.com>
2024-12-19 13:50:49 +0700

    chore: change photo to gif

--- src/components/modules/LandingModule/sections/AboutMeSection.tsx
+++ src/components/modules/LandingModule/sections/AboutMeSection.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/elements';
 import Image from 'next/image'
 import Link from 'next/link';
+import { useState } from 'react';
 import { useWindowSize } from 'usehooks-ts';
 
 export const AboutMeSection = () => {
   const { width } = useWindowSize()
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <div id='home-header' className={`pt-32 px-4 md:pt-40`}>
@@ -26,7 +28,7 @@ export const AboutMeSection = () => {
             Let's <span className='text-purple-400'> Empower</span> Ideas Through Design!
           </h1>
           <p className='text-white w-full text-sm mx-4 pb-4 md:mr-24 md:mx-auto xl:mx-0 text-justify pt-2'>
-          I'm Rania Maharani Narendra, currently diving deep into Computer Science at the University of Indonesia, I'm on a creative journey where tech meets design. Fueled by a passion for UI/UX, Graphic Design, and Software Development, I turn ideas into impactful, user-friendly experiences!
+            I'm Rania Maharani Narendra, currently diving deep into Computer Science at the University of Indonesia, I'm on a creative journey where tech meets design. Fueled by a passion for UI/UX, Graphic Design, and Software Development, I turn ideas into impactful, user-friendly experiences!
           </p>
           <div className='flex-row pb-6 md:pb-16 flex gap-8 w-full justify-center md:justify-start'>
             <Link href="#contact">
@@ -38,12 +40,21 @@ export const AboutMeSection = () => {
           </div>
         </div>
         <div className='mx-auto relative bottom-0 w-[70%] md:w-[50%] max-w-96 aspect-[4.2/5] overflow-hidden'>
+          {isLoading && (
+            <Image
+              src="/images/still.png" 
+              alt="Loading"
+              layout="fill"
+              objectFit="cover"
+            />
+          )}
           <Image
-            src={'/images/portrait2.png'}
-            alt={'Raniaarn'}
+            src="/images/portrait2.gif"
+            alt="Raniaarn"
             layout="fill"
-          >
-          </Image>
+            objectFit="cover"
+            onLoadingComplete={() => setIsLoading(false)}
+          />
         </div>
       </div>
     </div>
